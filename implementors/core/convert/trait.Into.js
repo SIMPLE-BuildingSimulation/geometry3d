49ba38d8a30b1c1b8a79809c7d9331d6764c2ec3
germolinal <germolinal@users.noreply.github.com>
2022-10-10 02:18:57 +0000

    Deploying to gh-pages from @ SIMPLE-BuildingSimulation/geometry3d@62768191b91bfb19f53645647b10a206ba9f675b 🚀

--- implementors/core/convert/trait.Into.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["geometry3d"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.64.0/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"geometry3d/vector3d/struct.Vector3D.html\" title=\"struct geometry3d::vector3d::Vector3D\">Vector3D</a>&gt; for <a class=\"struct\" href=\"geometry3d/point3d/struct.Point3D.html\" title=\"struct geometry3d::point3d::Point3D\">Point3D</a>","synthetic":false,"types":["geometry3d::point3d::Point3D"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
